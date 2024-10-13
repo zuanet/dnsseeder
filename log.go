b0970107ad63fd9d0657138e52de9391b450fcbe
zuanet <168155305+zuanet@users.noreply.github.com>
2024-10-13 10:09:11 +0300

    Add files via upload

---
+++ log.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/zuanet/zuad/infrastructure/logger"
+	"github.com/zuanet/zuad/util/panics"
+)
+
+var (
+	backendLog = logger.NewBackend()
+	log        = backendLog.Logger("SEED")
+	spawn      = panics.GoroutineWrapperFunc(log)
+)
+
+func initLog(noLogFiles bool, logLevel, logFile, errLogFile string) {
+	level, ok := logger.LevelFromString(logLevel)
+	if !ok {
+		fmt.Fprintf(os.Stderr, "Invalid loglevel: %s", logLevel)
+		os.Exit(1)
+	}
+	err := backendLog.AddLogWriter(os.Stdout, level)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error adding stdout to the logger for level %s: %s", logger.LevelWarn, err)
+		os.Exit(1)
+	}
+
+	if !noLogFiles {
+		err = backendLog.AddLogFile(logFile, logger.LevelTrace)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error adding log file %s as log rotator for level %s: %s", logFile, logger.LevelTrace, err)
+			os.Exit(1)
+		}
+		err = backendLog.AddLogFile(errLogFile, logger.LevelWarn)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error adding log file %s as log rotator for level %s: %s", errLogFile, logger.LevelWarn, err)
+			os.Exit(1)
+		}
+	}
+
+	err = backendLog.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error starting the logger: %s ", err)
+		os.Exit(1)
+	}
+
+	log.SetLevel(logger.LevelDebug)
+}
